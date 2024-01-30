98c0693e12330705cad40eb9e179de38200b94ad
1206Priyanka <43996054+1206Priyanka@users.noreply.github.com>
2024-01-30 10:08:36 +0000

    Update finalExam.R

--- finalExam.R
+++ finalExam.R
@@ -1,9 +1,4 @@
-#############################
-# random.txt for Resit Exam #
-#############################
 
-# Code for Exercise 3
-# Remember to replace studentid with your STUDENTID.  If you are student id is C12345678 use only the numeric part 12345678
 dataEx3a<-c(18.94,  4.64,  1.80,  2.26,  4.17,  4.93,  4.26, 4.80,  3.70,  2.77,  8.87,  9.82,  5.50,  4.74, 8.67, 3.78,  8.54,  2.62,  2.10,  4.45,  4.02,  3.82,  5.54, 3.13, 
             10.96,  2.73,  2.56,  3.41, 9.56,  3.21)
 dataEx3b<-c(8.16,  5.26,  3.80,  4.43,  4.94,  3.73,  4.54, 3.78,  7.40,  23.21,  4.25,  4.44, 12.31, 10.27, 5.39, 2.15,  6.91,  3.80,  8.42, 16.34,  5.36,  5.19,  3.72, 10.27, 
@@ -19,8 +14,7 @@ mysample2
 
 
 
-# Code for Exercise 4
-# Remember to replace studentid with your STUDENTID.  If you are student id is C12345678 use only the numeric part 12345678
+
 dataEx3x<- c(16, 1, 4,  13, 14,  8,  8,  8,  9,  3,  0,  7, 1,  1, 18, 3,  2,  2, 14, 13, 16, 10, 19,  2, 1, 14, 10, 16, 6, 0)
 dataEx3y <- c(83, 17, 40, 72, 82, 53, 51, 51, 58, 33, 17, 49, 23, 26, 94, 31, 27, 28, 81, 74, 85, 54, 95, 29, 25, 73, 65, 83, 49, 28)
 set.seed(22074213) 
@@ -59,8 +53,7 @@ print(anova_table)
       
 
 # Code for PART B - Exercise 5. 
-# Remember to read the data into a dataframe or matrix called "cars" for the code to work 
-# Remember to replace studentid with your STUDENTID.  If you are student id is C12345678 use only the numeric part 12345678
+
 set.seed(22074213) 
 k<-sample(1:392, 300)
 mycars<-cars[k,]
